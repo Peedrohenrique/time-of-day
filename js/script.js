5899c39c4cb987851379d689abd62f65142d6bfc
Pedro Henrique <80182753+Peedrohenrique@users.noreply.github.com>
2021-07-14 16:11:10 -0300

    Time of day Project 1.2
    Adicionado imagens, formatação da imagens, e formatação da hora

--- js/script.js
+++ js/script.js
@@ -3,18 +3,34 @@ function carregar(){
     var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
-    msg.innerHTML = `Agora são ${hora} horas.`
+    var min = data.getMinutes()
+    var seg = data.getSeconds()
+    
+
+    if (hora < 10) {
+        hora = '0' + hora;
+    } else if (min < 10) {
+        min = '0' + min;
+    } else if (seg < 10) {
+        seg = '0' + seg;
+    } else {
+
+    }
+
     if ( hora >= 0 && hora < 12){
         //BOM DIA!
-        img.src = ''
-        document.body.style.background = ''
+        img.src = '/img/manha.jpg'
+        document.body.style.background = '#feb408b2'
     } else if (hora >= 12 && hora <= 18) {
         //BOA TARDE!
-        img.src = ''
-        document.body.style.background = ''
+        img.src = '/img/tarde.jpeg'
+        document.body.style.background = '#c1a18b'
     } else {
         //BOA NOITE!
-        img.src = ''
-        document.body.style.background = ''
+        img.src = '/img/noite.jpg'
+        document.body.style.background = '#261f25'
     }
+    msg.innerHTML = `${hora}:${min}:${seg}`
+    
+    setInterval(carregar, 1000);
 }
